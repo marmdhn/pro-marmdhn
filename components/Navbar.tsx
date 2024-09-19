e76263c6554ea5056d6c39410186b38cbdf59cde
Ramadhan <makbarramadhan62@gmail.com>
2024-09-19 22:30:05 +0700

    refactor: implement react scroll

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,29 +1,30 @@
 "use client";
 
-import Link from "next/link";
 import Image from "next/image";
-import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
+import { Link } from "react-scroll";
 import PrimaryButton from "@/components/PrimaryButton";
 
 const Navbar = () => {
-  const pathname = usePathname();
-  const [activePath, setActivePath] = useState("");
   const [scrollDirection, setScrollDirection] = useState("up");
-
-  useEffect(() => {
-    setActivePath(pathname);
-  }, [pathname]);
+  const [hasShadow, setHasShadow] = useState(false);
 
   useEffect(() => {
     let lastScrollY = window.scrollY;
 
     const handleScroll = () => {
       const currentScrollY = window.scrollY;
-      if (currentScrollY > lastScrollY) {
-        setScrollDirection("down"); // Scroll ke bawah
+
+      if (currentScrollY > 50) {
+        setHasShadow(true);
       } else {
-        setScrollDirection("up"); // Scroll ke atas
+        setHasShadow(false);
+      }
+
+      if (currentScrollY > lastScrollY) {
+        setScrollDirection("down");
+      } else {
+        setScrollDirection("up");
       }
       lastScrollY = currentScrollY;
     };
@@ -37,59 +38,58 @@ const Navbar = () => {
 
   return (
     <nav
-      className={`bg-transparent backdrop-blur-2xl transition-all duration-300 sticky top-0 z-50 overflow-hidden ${
+      className={`px-[50px] transition-all duration-300 sticky top-0 z-50 overflow-hidden ${
         scrollDirection === "down" ? "-translate-y-full" : "translate-y-0"
-      }`}
+      } ${
+        hasShadow ? "shadow-lg bg-[#1B1C21]/90" : "bg-transparent"
+      } backdrop-blur-sm`}
     >
       <div className="h-[25px] my-[25px] relative flex items-center justify-between">
         <div className="col-span-1 me-auto">
-          <Link href="/" className="hidden sm:block">
+          <Link
+            to="hero"
+            smooth={true}
+            duration={500}
+            className="hidden sm:block hover:cursor-pointer"
+          >
             <Image src="/logo.svg" alt="Logo" width={75} height={40} priority />
           </Link>
         </div>
         <div className="col-span-1">
-          <div className="space-x-4 justify-center hidden sm:flex">
+          <div className="space-x-4 justify-center items-center hidden sm:flex">
             <Link
-              href="/about"
-              className={`text-gray-900 px-3 pt-1 rounded-md text-xl font-semibold ${
-                activePath === "/about"
-                  ? "text-primary dark:text-primary"
-                  : "hover:text-primary dark:hover:text-primary dark:text-white"
-              } transition-all duration-200`}
+              to="about"
+              smooth={true}
+              duration={500}
+              className={`font-sf-mono text-white px-3 rounded-md text-sm font-semibold hover:text-primary transition-all duration-200 hover:cursor-pointer`}
             >
               About
             </Link>
             <Link
-              href="/projects"
-              className={`text-gray-900 px-3 pt-1 rounded-md text-xl font-semibold ${
-                activePath === "/projects"
-                  ? "text-primary dark:text-primary"
-                  : "hover:text-primary dark:hover:text-primary dark:text-white"
-              } transition-all duration-200`}
+              to="experience"
+              smooth={true}
+              duration={500}
+              className={`font-sf-mono text-white px-3 rounded-md text-sm font-semibold hover:text-primary transition-all duration-200 hover:cursor-pointer`}
             >
               Experience
             </Link>
             <Link
-              href="/gallery"
-              className={`text-gray-900 px-3 pt-1 rounded-md text-xl font-semibold ${
-                activePath === "/gallery"
-                  ? "text-primary dark:text-primary"
-                  : "hover:text-primary dark:hover:text-primary dark:text-white"
-              } transition-all duration-200`}
+              to="work"
+              smooth={true}
+              duration={500}
+              className={`font-sf-mono text-white px-3 rounded-md text-sm font-semibold hover:text-primary transition-all duration-200 hover:cursor-pointer`}
             >
               Work
             </Link>
             <Link
-              href="/gallery"
-              className={`text-gray-900 px-3 pt-1 rounded-md text-xl font-semibold ${
-                activePath === "/gallery"
-                  ? "text-primary dark:text-primary"
-                  : "hover:text-primary dark:hover:text-primary dark:text-white"
-              } transition-all duration-200`}
+              to="contact"
+              smooth={true}
+              duration={500}
+              className={`font-sf-mono text-white px-3 rounded-md text-sm font-semibold hover:text-primary transition-all duration-200 hover:cursor-pointer`}
             >
               Contact
             </Link>
-            <Link href="/gallery">
+            <Link href="/">
               <PrimaryButton btnText="Resume" />
             </Link>
           </div>
