b265a9c040e78bd370ef089e3d6f49ea2de95787
Ramadhan <makbarramadhan62@gmail.com>
2024-09-19 14:03:28 +0700

    feat: add footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+const Footer = () => {
+  return (
+    <footer className="relative bottom-0 w-full">
+      <div className="container mx-auto">
+        <div className="w-full mt-10">
+          <p className="font-medium text-xl text-secondary text-center">
+            Design & Built by Muhammad Akbar Ramadhan
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
