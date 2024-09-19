788f04adbd5c29b619039c4884f3f542d6073bbb
Ramadhan <makbarramadhan62@gmail.com>
2024-09-19 20:56:13 +0700

    feat: add component section 4

---
+++ components/Home/Section4.tsx
@@ -0,0 +1,19 @@
+const Section4 = () => {
+  return (
+    <section
+      id="section4"
+      className="section flex justify-center items-center relative w-full"
+    >
+      <div className="flex flex-col max-w-7xl">
+        <div className="flex items-center gap-10">
+          <h2 className="text-3xl inline-block font-sf-mono mb-0">
+            Some Things I’ve Built
+          </h2>
+          <div className="h-[1px] w-[375px] bg-secondary"></div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Section4;
