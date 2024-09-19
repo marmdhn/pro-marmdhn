ba125a34da07c7c479f9ffe7d87479d0bae01627
Ramadhan <makbarramadhan62@gmail.com>
2024-09-19 14:04:15 +0700

    refactor: implement footer component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,9 +15,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased px-[50px]`}>
+      <body className={`antialiased px-[50px] pb-[50px]`}>
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
