128074f487bf3ec0ea1498588129cf6adbb69af9
Ramadhan <makbarramadhan62@gmail.com>
2024-09-19 14:03:54 +0700

    style: adjust padding

--- components/PrimaryButton.tsx
+++ components/PrimaryButton.tsx
@@ -2,7 +2,7 @@ const PrimaryButton = ({ btnText }: { btnText: string }) => {
   return (
     <div className="bg-primary rounded-md">
       <div
-        className={`px-3 py-1 text-primary bg-background rounded-md text-lg font-semibold border border-primary hover:translate-y-[-0.25rem] hover:translate-x-[-0.25rem] transition-transform duration-400 will-change-transform`}
+        className={`px-3 pt-1 text-primary bg-background rounded-md text-xl font-semibold border border-primary hover:translate-y-[-0.25rem] hover:translate-x-[-0.25rem] transition-transform duration-400 will-change-transform`}
       >
         {btnText}
       </div>
