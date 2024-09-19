100e3ca818aa3454024d0838c6ea7c0d584a1170
Ramadhan <makbarramadhan62@gmail.com>
2024-09-19 22:29:40 +0700

    feat: add sticky element component

---
+++ components/Home/StickyElement.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa6";
+import { Link } from "react-scroll";
+
+const StickyElement = () => {
+  return (
+    <>
+      <div className="fixed bottom-[50px] z-50">
+        <div className="flex flex-col gap-4">
+          <FaInstagram
+            size={20}
+            className="text-secondary hover:text-white hover:scale-105 hover:cursor-pointer transition-all duration-300"
+          />
+          <FaLinkedin
+            size={20}
+            className="text-secondary hover:text-white hover:scale-105 hover:cursor-pointer transition-all duration-300"
+          />
+          <FaGithub
+            size={20}
+            className="text-secondary hover:text-white hover:scale-105 hover:cursor-pointer transition-all duration-300"
+          />
+        </div>
+      </div>
+
+      <Link
+        to="hero"
+        smooth={true}
+        duration={500}
+        className="fixed bottom-[100px] right-0 z-50 -rotate-90 italic font-sf-mono text-sm text-secondary hover:cursor-pointer"
+      >
+        <span className="underline underline-offset-8">Front End</span>{" "}
+        Developer
+      </Link>
+    </>
+  );
+};
+
+export default StickyElement;
