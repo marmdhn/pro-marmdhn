d206bce2e89a38556dce98ff1cd507f5c9778598
Ramadhan <makbarramadhan62@gmail.com>
2024-09-20 15:02:56 +0700

    feat: add circle shine component

---
+++ components/CircleShine.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+const CircleShine = () => {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 5 }}
+      className="absolute z-10 -top-48 left-[220px] w-[400px] h-[400px] bg-primary rounded-full blur-[300px] animate-slide-left-right"
+    ></motion.div>
+  );
+};
+
+export default CircleShine;
