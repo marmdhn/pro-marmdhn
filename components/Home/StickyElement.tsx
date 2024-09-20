0371ce7301adf84e0526fbe21315e45f2e3e0d07
Ramadhan <makbarramadhan62@gmail.com>
2024-09-20 19:27:42 +0700

    refactor: implement motion

--- components/Home/StickyElement.tsx
+++ components/Home/StickyElement.tsx
@@ -2,10 +2,15 @@
 
 import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa6";
 import { Link } from "react-scroll";
+import { motion } from "framer-motion";
 
 const StickyElement = () => {
   return (
-    <>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 100 }}
+      transition={{ duration: 1 }}
+    >
       <div className="fixed bottom-[50px] z-50">
         <div className="flex flex-col gap-4 text-white">
           <FaInstagram
@@ -32,7 +37,7 @@ const StickyElement = () => {
         <span className="underline underline-offset-8">Front End</span>{" "}
         Developer
       </Link>
-    </>
+    </motion.div>
   );
 };
 
