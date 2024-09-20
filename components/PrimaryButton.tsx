d22953082f308c65b020d24a5115f5937a72d853
Ramadhan <makbarramadhan62@gmail.com>
2024-09-20 10:02:47 +0700

    style: add hover style

--- components/PrimaryButton.tsx
+++ components/PrimaryButton.tsx
@@ -1,6 +1,6 @@
 const PrimaryButton = ({ btnText }: { btnText: string }) => {
   return (
-    <div className="bg-primary rounded-md">
+    <div className="bg-primary rounded-md hover:cursor-pointer">
       <div
         className={`px-3 p-2 font-sf-mono text-primary bg-background rounded-md text-sm font-semibold border border-primary hover:translate-y-[-0.25rem] hover:translate-x-[-0.25rem] transition-transform duration-400 will-change-transform`}
       >
