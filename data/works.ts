f5d32bc97627f51f98e58dd6218e493e4fc5c8f3
Ramadhan <makbarramadhan62@gmail.com>
2024-09-20 10:01:28 +0700

    feat: add work data

---
+++ data/works.ts
@@ -0,0 +1,29 @@
+interface IWorks {
+  imgUrl: string;
+  title: string;
+  description: string;
+  techStack: string[];
+  githubUrl: string;
+  websiteUrl: string;
+}
+
+export const Works: IWorks[] = [
+  {
+    imgUrl: "ppdb-telkomschools.png",
+    title: "PPDB TelkomSchools",
+    description:
+      "The PPDB Telkom Schools website streamlines the registration process for Telkom Schools in Indonesia, offering a user-friendly interface for easy application, document submission, and status tracking.",
+    techStack: ["NextJs", "CSS", "Bootstrap"],
+    githubUrl: "",
+    websiteUrl: "https://ppdb.telkomschools.sch.id/",
+  },
+  {
+    imgUrl: "ppdb-telkomschools.png",
+    title: "PPDB TelkomSchools",
+    description:
+      "The PPDB Telkom Schools website streamlines the registration process for Telkom Schools in Indonesia, offering a user-friendly interface for easy application, document submission, and status tracking.",
+    techStack: ["NextJs", "CSS", "Bootstrap"],
+    githubUrl: "",
+    websiteUrl: "https://ppdb.telkomschools.sch.id/",
+  },
+];
