56f2b03d9509cf108edbeb2e3fe1bee31604f753
Ramadhan <makbarramadhan62@gmail.com>
2024-09-20 15:02:48 +0700

    style: adjust icon size & color

--- components/Home/StickyElement.tsx
+++ components/Home/StickyElement.tsx
@@ -7,18 +7,18 @@ const StickyElement = () => {
   return (
     <>
       <div className="fixed bottom-[50px] z-50">
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-4 text-white">
           <FaInstagram
-            size={20}
-            className="text-secondary hover:text-white hover:scale-105 hover:cursor-pointer transition-all duration-300"
+            size={24}
+            className="hover:text-primary hover:-translate-y-0.5 hover:cursor-pointer transition-all duration-300"
           />
           <FaLinkedin
-            size={20}
-            className="text-secondary hover:text-white hover:scale-105 hover:cursor-pointer transition-all duration-300"
+            size={24}
+            className="hover:text-primary hover:-translate-y-0.5 hover:cursor-pointer transition-all duration-300"
           />
           <FaGithub
-            size={20}
-            className="text-secondary hover:text-white hover:scale-105 hover:cursor-pointer transition-all duration-300"
+            size={24}
+            className="hover:text-primary hover:-translate-y-0.5 hover:cursor-pointer transition-all duration-300"
           />
         </div>
       </div>
@@ -27,7 +27,7 @@ const StickyElement = () => {
         to="hero"
         smooth={true}
         duration={500}
-        className="fixed bottom-[100px] right-0 z-50 -rotate-90 italic font-sf-mono text-sm text-secondary hover:cursor-pointer hover:text-white hover:scale-110 transition-all duration-300"
+        className="fixed bottom-[100px] right-0 z-50 -rotate-90 italic font-sf-mono text-sm text-white hover:cursor-pointer hover:text-primary hover:-translate-y-0.5 transition-all duration-300"
       >
         <span className="underline underline-offset-8">Front End</span>{" "}
         Developer
