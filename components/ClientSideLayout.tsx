7eeada0ba4361a8084274998fbc1360d16588652
Ramadhan <makbarramadhan62@gmail.com>
2024-09-20 11:14:20 +0700

    feat: add client side layout

---
+++ components/ClientSideLayout.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useState, useEffect, ReactNode } from "react";
+import Loading from "@/components/Loading";
+
+interface ClientSideLayoutProps {
+  children: ReactNode;
+}
+
+export default function ClientSideLayout({ children }: ClientSideLayoutProps) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isFadingOut, setIsFadingOut] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsFadingOut(true);
+      setTimeout(() => setIsLoading(false), 1000);
+    }, 1500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) {
+    return <Loading isFadingOut={isFadingOut} />;
+  }
+
+  return <>{children}</>;
+}
