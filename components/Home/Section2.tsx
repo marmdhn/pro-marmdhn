87422f05ad6b21a56b2fefac5ccf9aece082401b
Ramadhan <makbarramadhan62@gmail.com>
2024-09-22 15:10:15 +0700

    refactor: implement on scroll animation

--- components/Home/Section2.tsx
+++ components/Home/Section2.tsx
@@ -1,32 +1,77 @@
+"use client";
+
+import { useEffect } from "react";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 import { skills } from "@/data/skills";
 import { BsTriangleFill } from "react-icons/bs";
 import Image from "next/image";
 
 const Section2 = () => {
+  const controls = useAnimation();
+  const { ref, inView } = useInView({
+    threshold: 0.5,
+  });
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("show");
+    }
+  }, [controls, inView]);
+
+  const container = {
+    hidden: { opacity: 0, x: 50 },
+    show: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        staggerChildren: 0.5,
+        duration: 0.75,
+      },
+    },
+  };
+
+  const item = {
+    hidden: { opacity: 0, y: 50 },
+    show: { opacity: 1, y: 0, transition: { duration: 0.75, delay: 0.5 } },
+  };
+
   return (
     <section
       id="about"
       className="section flex justify-center items-center py-20"
+      ref={ref as React.LegacyRef<HTMLElement>}
     >
       <div className="max-w-7xl text-white py-10 px-16">
-        <div className="flex items-center">
+        <motion.div
+          className="flex items-center"
+          initial="hidden"
+          animate={controls}
+          variants={container}
+        >
           <div className="md:w-2/3 flex flex-col gap-5 text-lg">
             <div className="flex items-center gap-10">
-              <h2 className="text-3xl inline-block font-sf-mono mb-0">
+              <motion.h2
+                className="text-3xl inline-block font-sf-mono mb-0"
+                variants={item}
+              >
                 About <span className="text-primary">Me</span>
-              </h2>
-              <div className="h-[1px] w-[500px] bg-secondary"></div>
+              </motion.h2>
+              <motion.div
+                className="h-[1px] w-[500px] bg-secondary"
+                variants={item}
+              ></motion.div>
             </div>
 
-            <div className="flex flex-col gap-5">
-              <span>
+            <motion.div className="flex flex-col gap-5" variants={item}>
+              <motion.span variants={item}>
                 Hello! My name is Muhammad Akbar Ramadhan, and I have a passion
                 for creating intuitive and responsive web applications. My
                 journey into web development began during my studies at ITTelkom
                 Surabaya, where I honed my skills in HTML, CSS, and JavaScript
                 by experimenting with various projects.
-              </span>
-              <span>
+              </motion.span>
+              <motion.span variants={item}>
                 Fast-forward to today, I’ve had the privilege of working as a{" "}
                 <span className="font-bold text-primary">
                   Front End Developer at Yayasan Pendidikan Telkom
@@ -35,24 +80,35 @@ const Section2 = () => {
                 also gained valuable experience working at PT. Akhdani Reka
                 Solusi as a Software Engineer, collaborating with diverse teams
                 to deliver high-quality software solutions.
-              </span>
-              <span>
+              </motion.span>
+              <motion.span variants={item}>
                 Currently, I focus on creating responsive, high-performance web
                 applications using modern technologies such as React.js, Vue.js,
                 and Tailwind CSS, while ensuring a seamless user experience.
-              </span>
-            </div>
-            <div className="flex gap-5 flex-wrap">
+              </motion.span>
+            </motion.div>
+
+            <motion.div
+              className="flex gap-5 flex-wrap"
+              initial="hidden"
+              animate={controls}
+              variants={container}
+            >
               {skills.map((skill, index) => (
-                <div key={index} className="flex gap-1">
+                <motion.div key={index} className="flex gap-1" variants={item}>
                   <BsTriangleFill className="rotate-90 text-primary" />
                   <span className="italic">{skill}</span>
-                </div>
+                </motion.div>
               ))}
-            </div>
+            </motion.div>
           </div>
 
-          <div className="md:w-1/3 flex justify-center">
+          <motion.div
+            className="md:w-1/3 flex justify-center"
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 1 }}
+          >
             <Image
               className="rounded-lg shadow-lg w-full h-auto max-w-xs"
               src="/image.png"
@@ -61,8 +117,8 @@ const Section2 = () => {
               height={100}
               unoptimized
             />
-          </div>
-        </div>
+          </motion.div>
+        </motion.div>
       </div>
     </section>
   );
--- components/Home/Section3.tsx
+++ components/Home/Section3.tsx
@@ -1,6 +1,55 @@
+"use client";
+
 import { Experiences } from "@/data/experiences";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
 
 const Section3 = () => {
+  const controls = useAnimation();
+  const { ref, inView } = useInView({
+    threshold: 0.5,
+  });
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+  }, [controls, inView]);
+
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.5,
+      },
+    },
+  };
+
+  const itemVariants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.6,
+        ease: "easeOut",
+      },
+    },
+  };
+
+  const hrVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        duration: 1,
+        ease: "easeIn",
+      },
+    },
+  };
+
   return (
     <section
       id="experience"
@@ -10,50 +59,65 @@ const Section3 = () => {
         <h2 className="text-3xl inline-block font-sf-mono mb-0">
           Where I’ve <span className="text-primary">Worked</span>
         </h2>
-        <div className="h-[1px] w-[300px] bg-secondary"></div>
+        <motion.div
+          className="h-[1px] w-[300px] bg-secondary"
+          variants={hrVariants} // Apply only opacity transition to hr
+          initial="hidden"
+          animate={controls}
+        />
       </div>
 
-      <div className="max-w-4xl">
+      <motion.div
+        className="max-w-4xl"
+        ref={ref as React.LegacyRef<HTMLDivElement>}
+        variants={containerVariants}
+        initial="hidden"
+        animate={controls}
+      >
         <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
-          {Experiences.map((experience, index) => (
-            <li key={index}>
-              <div className="timeline-middle">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  className="h-5 w-5"
+          {Experiences.map((experience, index) => {
+            const isLeft = index % 2 === 0;
+
+            return (
+              <motion.li key={index} variants={itemVariants}>
+                <div className="timeline-middle">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="h-5 w-5"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </div>
+                <div
+                  className={`timeline-${
+                    isLeft ? "start me-2" : "end ms-2"
+                  } mt-1.5`}
                 >
-                  <path
-                    fillRule="evenodd"
-                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </div>
-              <div
-                className={`timeline-${
-                  index % 2 === 0 ? "start me-2" : "end ms-2"
-                } mb-10 mt-1.5`}
-              >
-                <div className={`text-${index % 2 === 0 ? "end" : "start"}`}>
-                  <time className="font-mono italic">{experience.year}</time>
-                  <div className="text-xl font-bold">
-                    {experience.title}{" "}
-                    <span className="text-primary">
-                      @{experience.corporate}
-                    </span>
+                  <div className={`text-${isLeft ? "end" : "start"}`}>
+                    <time className="font-mono italic">{experience.year}</time>
+                    <div className="text-xl font-bold">
+                      {experience.title}{" "}
+                      <span className="text-primary">
+                        @{experience.corporate}
+                      </span>
+                    </div>
+                  </div>
+                  <div className="text-justify text-base text-text-secondary">
+                    {experience.description}
                   </div>
                 </div>
-                <div className="text-justify text-base text-text-secondary">
-                  {experience.description}
-                </div>
-              </div>
-              <hr className="bg-white" />
-            </li>
-          ))}
+                <motion.hr className="bg-white" variants={hrVariants} />
+              </motion.li>
+            );
+          })}
         </ul>
-      </div>
+      </motion.div>
     </section>
   );
 };
