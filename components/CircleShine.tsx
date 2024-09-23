37c1f72e9b2b141229da4185cfa2acd4a49e679f
Ramadhan <makbarramadhan62@gmail.com>
2024-09-23 10:57:44 +0700

    style: set hidden in small screen

--- components/CircleShine.tsx
+++ components/CircleShine.tsx
@@ -8,7 +8,7 @@ const CircleShine = () => {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 10 }}
-      className="absolute z-10 -top-48 left-[220px] w-[400px] h-[400px] bg-primary rounded-full blur-[300px] animate-slide-left-right"
+      className="absolute z-10  hidden lg:block -top-48 left-[220px] w-[400px] h-[400px] bg-primary rounded-full blur-[300px] animate-slide-left-right"
     ></motion.div>
   );
 };
