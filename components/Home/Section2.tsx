fc0bcf0c7e5fbddb8db0782a8bfa4e965bc1f436
Ramadhan <makbarramadhan62@gmail.com>
2024-09-23 11:17:51 +0700

    style: adjust responsive

--- components/Home/Section2.tsx
+++ components/Home/Section2.tsx
@@ -39,31 +39,35 @@ const Section2 = () => {
   return (
     <section
       id="about"
-      className="section flex justify-center items-center py-20"
+      className="section flex justify-center items-center py-10 lg:py-20"
       ref={ref as React.LegacyRef<HTMLElement>}
     >
-      <div className="max-w-7xl text-white py-10 px-16">
+      <div className="max-w-7xl text-white px-6 lg:px-16">
         <motion.div
-          className="flex items-center"
+          className="flex flex-col lg:flex-row items-center"
           initial="hidden"
           animate={controls}
           variants={container}
         >
-          <div className="md:w-2/3 flex flex-col gap-5 text-lg">
-            <div className="flex items-center gap-10">
+          {/* Text Section */}
+          <div className="lg:w-2/3 flex flex-col gap-5 text-base lg:text-lg">
+            <div className="flex items-center gap-5 lg:gap-10">
               <motion.h2
-                className="text-3xl inline-block font-sf-mono mb-0"
+                className="text-2xl lg:text-3xl font-sf-mono mb-0"
                 variants={item}
               >
                 About <span className="text-primary">Me</span>
               </motion.h2>
               <motion.div
-                className="h-[1px] w-[500px] bg-secondary"
+                className="h-[1px] w-[200px] lg:w-[300px] bg-secondary"
                 variants={item}
               ></motion.div>
             </div>
 
-            <motion.div className="flex flex-col gap-5" variants={item}>
+            <motion.div
+              className="flex flex-col gap-3 lg:gap-5"
+              variants={item}
+            >
               <motion.span variants={item}>
                 Hello! My name is Muhammad Akbar Ramadhan, and I have a passion
                 for creating intuitive and responsive web applications. My
@@ -103,8 +107,9 @@ const Section2 = () => {
             </motion.div>
           </div>
 
+          {/* Image Section */}
           <motion.div
-            className="md:w-1/3 flex justify-center"
+            className="lg:w-1/3 flex justify-center mt-10 lg:mt-0 ms-4 xl:ms-0"
             initial={{ opacity: 0, scale: 0.8 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{ duration: 1 }}
