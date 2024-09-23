a724e0c0d67c2bacadb14fcb0e95006ac19fd078
Ramadhan <makbarramadhan62@gmail.com>
2024-09-23 10:57:30 +0700

    refactor: add props icons

--- components/PrimaryButton.tsx
+++ components/PrimaryButton.tsx
@@ -1,10 +1,16 @@
-const PrimaryButton = ({ btnText }: { btnText: string }) => {
+interface PrimaryButtonProps {
+  btnText?: string;
+  icon?: React.ReactNode;
+}
+
+const PrimaryButton = ({ btnText, icon }: PrimaryButtonProps) => {
   return (
     <div className="bg-primary rounded-md hover:cursor-pointer">
       <div
-        className={`px-3 p-2 font-sf-mono text-primary bg-background rounded-md text-sm font-semibold border border-primary hover:translate-y-[-0.25rem] hover:translate-x-[-0.25rem] transition-transform duration-400 will-change-transform`}
+        className={`flex items-center justify-center space-x-2 px-3 p-2 font-sf-mono text-primary bg-background rounded-md text-sm font-semibold border border-primary hover:translate-y-[-0.25rem] hover:translate-x-[-0.25rem] transition-transform duration-400 will-change-transform`}
       >
-        {btnText}
+        {icon && <span>{icon}</span>}
+        {btnText && <span>{btnText}</span>}
       </div>
     </div>
   );
