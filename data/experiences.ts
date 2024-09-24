403fc74522cb5d6be319cf16fbb728ed1ab1af17
Ramadhan <makbarramadhan62@gmail.com>
2024-09-24 21:06:10 +0700

    refactor: adjust data

--- data/experiences.ts
+++ data/experiences.ts
@@ -11,20 +11,20 @@ export const Experiences: IExperience[] = [
     title: "Front End Developer",
     corporate: "Telkom Foundation",
     description:
-      "I was responsible for developing responsive and visually engaging user interfaces for web applications, with a primary focus on those used by Telkom Schools. In this role, I worked on optimizing performance by improving the code, reducing load times, and addressing various performance aspects to ensure a smooth and seamless user experience.",
+      "I was responsible for developing responsive user interfaces for Telkom Schools' web applications, optimizing performance, and ensuring a seamless user experience. I collaborated with UI/UX, backend, and DevOps teams to maintain smooth operations and aligned the application flow with business requirements.",
   },
   {
-    year: 2024,
-    title: "Front End Developer",
-    corporate: "Telkom Foundation",
+    year: 2023,
+    title: "Software Engineer",
+    corporate: "PT. Akhdani Reka Solusi",
     description:
-      "I was responsible for developing responsive and visually engaging user interfaces for web applications, with a primary focus on those used by Telkom Schools. In this role, I worked on optimizing performance by improving the code, reducing load times, and addressing various performance aspects to ensure a smooth and seamless user experience.",
+      "I was responsible for designing, developing, and maintaining software in line with the business needs or project requirements. My role also involved collaborating closely with a cross-functional team of developers, designers, and project managers to ensure that projects were completed on schedule and met their objectives.",
   },
   {
-    year: 2024,
-    title: "Front End Developer",
-    corporate: "Telkom Foundation",
+    year: 2022,
+    title: "Mobile Apps Develope (Intern)",
+    corporate: "PT. Otak Kanan",
     description:
-      "I was responsible for developing responsive and visually engaging user interfaces for web applications, with a primary focus on those used by Telkom Schools. In this role, I worked on optimizing performance by improving the code, reducing load times, and addressing various performance aspects to ensure a smooth and seamless user experience.",
+      "I developed an Android application to streamline document management for users. I proactively optimized the app's performance to ensure responsiveness and fast document processing. I also worked closely with the team to ensure project alignment and meet deadlines.",
   },
 ];
--- data/works.ts
+++ data/works.ts
@@ -18,12 +18,21 @@ export const Works: IWorks[] = [
     websiteUrl: "https://ppdb.telkomschools.sch.id/",
   },
   {
-    imgUrl: "ppdb-telkomschools.png",
-    title: "PPDB TelkomSchools",
+    imgUrl: "digits-telkomschools.png",
+    title: "DigiTS TelkomSchools",
     description:
-      "The PPDB Telkom Schools website streamlines the registration process for Telkom Schools in Indonesia, offering a user-friendly interface for easy application, document submission, and status tracking.",
-    techStack: ["NextJs", "CSS", "Bootstrap"],
+      "The DigiTS platform simplifies the re-registration process for students, while also providing comprehensive management tools for schools, covering everything from student and teacher data to academic, curriculum, and department management.",
+    techStack: ["NuxtJs", "CSS", "Bootstrap"],
     githubUrl: "",
-    websiteUrl: "https://ppdb.telkomschools.sch.id/",
+    websiteUrl: "https://digits.telkomschools.sch.id/",
+  },
+  {
+    imgUrl: "baitul-adab-islamic-school.png",
+    title: "Baitul Adab Islamic School Website",
+    description:
+      "The Baitul Adab Islamic School website serves as a comprehensive landing page, providing key information on admission requirements, facilities, and school activities. It also features an online registration form for prospective students, making the enrollment process easy and accessible.",
+    techStack: ["ReactJs", "TailwindCss", "DaisyUI"],
+    githubUrl: "",
+    websiteUrl: "https://digits.telkomschools.sch.id/",
   },
 ];
