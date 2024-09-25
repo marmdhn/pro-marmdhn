2ca4efa37e7cd5645b95cd3b2f8817a379fa210a
Ramadhan <makbarramadhan62@gmail.com>
2024-09-25 07:14:07 +0700

    refactor: adjust data & add id property

--- data/archive.ts
+++ data/archive.ts
@@ -1,4 +1,5 @@
 interface IArchive {
+  id: number;
   year: number;
   title: string;
   madeAt: string;
@@ -8,13 +9,36 @@ interface IArchive {
 
 export const Archives: IArchive[] = [
   {
+    id: 1,
     year: 2024,
-    title: "DigiTS",
+    title: "DigiTs Telkom Schools",
     madeAt: "Telkom Foundation",
+    buildWith: ["Nuxt.Js", "Vue.Js", "CSS", "Bootstrap"],
+    link: {
+      gitHubUrl: "",
+      webUrl: "https://digits.telkomschools.sch.id/",
+    },
+  },
+  {
+    id: 2,
+    year: 2024,
+    title: "School Profile Website",
+    madeAt: "Baitul Adab Islamic School",
     buildWith: ["Nuxt.Js", "Vue.Js", "Bootstrap"],
     link: {
       gitHubUrl: "",
-      webUrl: "",
+      webUrl: "https://digits.telkomschools.sch.id/",
+    },
+  },
+  {
+    id: 3,
+    year: 2024,
+    title: "PPDB Telkom Schools",
+    madeAt: "Telkom Foundation",
+    buildWith: ["Next.Js", "React.Js", "CSS", "Bootstrap"],
+    link: {
+      gitHubUrl: "",
+      webUrl: "https://ppdb.telkomschools.sch.id/",
     },
   },
 ];
