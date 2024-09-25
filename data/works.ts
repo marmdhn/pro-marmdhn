39a5cfd52dafe10858239226b134a94312cbf0ee
Ramadhan <makbarramadhan62@gmail.com>
2024-09-25 07:14:24 +0700

    refactor: adjust data

--- data/works.ts
+++ data/works.ts
@@ -10,19 +10,19 @@ interface IWorks {
 export const Works: IWorks[] = [
   {
     imgUrl: "ppdb-telkomschools.png",
-    title: "PPDB TelkomSchools",
+    title: "PPDB Telkom Schools",
     description:
       "The PPDB Telkom Schools website streamlines the registration process for Telkom Schools in Indonesia, offering a user-friendly interface for easy application, document submission, and status tracking.",
-    techStack: ["NextJs", "CSS", "Bootstrap"],
+    techStack: ["Next.Js", "React.Js", "CSS", "Bootstrap"],
     githubUrl: "",
     websiteUrl: "https://ppdb.telkomschools.sch.id/",
   },
   {
     imgUrl: "digits-telkomschools.png",
-    title: "DigiTS TelkomSchools",
+    title: "DigiTS Telkom Schools",
     description:
       "The DigiTS platform simplifies the re-registration process for students, while also providing comprehensive management tools for schools, covering everything from student and teacher data to academic, curriculum, and department management.",
-    techStack: ["NuxtJs", "CSS", "Bootstrap"],
+    techStack: ["Nuxt.Js", "Vue.Js", "CSS", "Bootstrap"],
     githubUrl: "",
     websiteUrl: "https://digits.telkomschools.sch.id/",
   },
@@ -31,7 +31,7 @@ export const Works: IWorks[] = [
     title: "Baitul Adab Islamic School Website",
     description:
       "The Baitul Adab Islamic School website serves as a comprehensive landing page, providing key information on admission requirements, facilities, and school activities. It also features an online registration form for prospective students, making the enrollment process easy and accessible.",
-    techStack: ["ReactJs", "TailwindCss", "DaisyUI"],
+    techStack: ["React.Js", "TailwindCss", "DaisyUI"],
     githubUrl: "",
     websiteUrl: "https://digits.telkomschools.sch.id/",
   },
