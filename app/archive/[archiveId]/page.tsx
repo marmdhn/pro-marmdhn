1be9e01919e5d9f8dbb9ab316bb07a8e8d1b0cd0
Ramadhan <makbarramadhan62@gmail.com>
2024-12-15 15:21:41 +0700

    refactor: add detail photo mapping

--- app/archive/[archiveId]/page.tsx
+++ app/archive/[archiveId]/page.tsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from "react";
 import { IArchive, Archives } from "@/data/archive";
 import { motion } from "framer-motion";
 import Image from "next/image";
+import Zoom from "react-medium-image-zoom";
+import "react-medium-image-zoom/dist/styles.css";
 
 const DetailArchive = ({ params }: { params: { archiveId: string } }) => {
   const [detailData, setDetailData] = useState<IArchive | null>(null);
@@ -39,12 +41,12 @@ const DetailArchive = ({ params }: { params: { archiveId: string } }) => {
   return (
     <div className="section flex flex-col justify-center items-center pt-4 pb-16">
       <motion.div
-        className="w-full flex flex-col lg:flex-row justify-center items-center gap-2 lg:gap-10"
+        className="w-full flex flex-col lg:flex-row justify-center items-center gap-2 lg:gap-10 section"
         initial="hidden"
         animate="visible"
         variants={containerVariants}
       >
-        <div className="flex flex-col items-center lg:items-start">
+        <div className="w-1/3 flex flex-col items-center lg:items-start">
           <div className="flex items-center gap-5">
             <motion.h2
               className="text-3xl font-bold"
@@ -60,11 +62,11 @@ const DetailArchive = ({ params }: { params: { archiveId: string } }) => {
         </div>
 
         <motion.div
-          className={`w-full lg:w-[${detailData?.imageCover?.width}%] relative flex justify-center lg:mt-0`}
+          className={`w-2/3 relative flex justify-center lg:mt-0`}
           variants={imageVariants}
         >
           <Image
-            className="w-full h-auto object-cover hover:scale-105 duration-500 transition-all ease-in-out"
+            className="w-auto h-auto object-cover hover:scale-105 duration-500 transition-all ease-in-out rounded-lg"
             src={`/archive/${detailData?.imageCover?.fileUrl}`}
             alt="Profile Image"
             width={100}
@@ -75,6 +77,40 @@ const DetailArchive = ({ params }: { params: { archiveId: string } }) => {
           />
         </motion.div>
       </motion.div>
+
+      <div className="mt-16">
+        {detailData?.detailPhoto?.length > 0 && (
+          <div className="flex flex-col items-center">
+            <motion.h2
+              className="text-3xl font-bold mb-8"
+              variants={containerVariants}
+            >
+              Detail Images
+            </motion.h2>
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-2 gap-4">
+              {detailData?.detailPhoto?.map((item, index) => (
+                <div
+                  key={index}
+                  className="group relative overflow-hidden rounded-lg"
+                >
+                  <Zoom zoomMargin={40}>
+                    <Image
+                      className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
+                      src={`/archive/${item}`}
+                      alt={`Image ${index + 1}`}
+                      width={300}
+                      height={300}
+                      draggable="false"
+                      onContextMenu={(e) => e.preventDefault()}
+                      unoptimized
+                    />
+                  </Zoom>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
