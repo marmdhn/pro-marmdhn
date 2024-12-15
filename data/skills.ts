6bdd8a6cf5de651f4b4567e5efd423f242a49cd8
Ramadhan <makbarramadhan62@gmail.com>
2024-12-15 16:26:18 +0700

    refactor: add skill

--- data/skills.ts
+++ data/skills.ts
@@ -1,11 +1,16 @@
 export const skills = [
+  // Frontend frameworks & libraries
   "JavaScript (ES6+)",
   "TypeScript",
   "React",
   "Vue",
   "Next.js",
   "Nuxt.js",
+
+  // Backend frameworks
   ".NET (Blazor)",
+
+  // UI/UX design & styling
   "Tailwind CSS",
   "Bootstrap",
   "Figma",
