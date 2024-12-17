96c9a2d58c97839d9a945de0849dc34625e944d9
Ramadhan <makbarramadhan62@gmail.com>
2024-12-17 15:09:56 +0700

    refactor: remove unused component

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,11 @@ import Section3 from "@/components/Home/Section3";
 import StickyElement from "@/components/Home/StickyElement";
 import Section5 from "@/components/Home/Section5";
 import CircleShine from "@/components/CircleShine";
-import ElasticEffect from "@/components/ElastifEffect";
 
 export default function Home() {
   return (
     <div className="px-8 xl:px-0 mx-auto h-full overflow-x-hidden">
       <CircleShine />
-      <ElasticEffect />
       <Section1 />
       <Section2 />
       <Section3 />
