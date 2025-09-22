80e821118308807cab95d5871d4b2c571202b535
marmdhn <makbarramadhan62@gmail.com>
2025-09-22 08:46:15 +0700

    refactor: Update resume link in Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -225,7 +225,7 @@ const Navbar = () => {
 
           <Link
             target="_blank"
-            href="https://drive.google.com/file/d/1HDHMp36Hx9q5tLQQZjtFE-RvohDncExR/view?usp=drive_link"
+            href="https://drive.google.com/file/d/13uc35WSy9qa4x8B8TEgiFNhN9KqWJoUb/view?usp=sharing"
           >
             <PrimaryButton btnText="" icon={<FaFileAlt size={20} />} />
           </Link>
